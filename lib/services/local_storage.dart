710f3a366e31126304bbf55b464378426e407764
yuvanchanthar <yuvanpraveen182@gmail.com>
2025-11-11 09:57:02 +0530

    initial commit

---
+++ lib/services/local_storage.dart
@@ -0,0 +1,43 @@
+class Note{
+  String id;
+  String title;
+  String content;
+  DateTime createdAt;
+
+  Note({
+    required this.id,required this.title,required this.content,required this.createdAt
+  });
+
+  Map<String, dynamic> toJson(){
+    return{
+      'id':id,
+      'title': title,
+      'content':content,
+      'createdAt':createdAt.toIso8601String()
+
+    };
+  }
+  factory Note.fromJson(Map<String,dynamic>json){
+    return Note(id: json['id'],
+     title: json['title'],
+      content: json['content'],
+       createdAt: DateTime.parse(json['createdAt']));
+
+  }
+  Note CopyWith({
+    String? id,
+    String?title,
+    String?content,
+    DateTime?createdAt
+  })
+  {
+    return Note(id: id?? this.id,
+   title: title ?? this.title,
+    content: content ?? this.content,
+     createdAt: createdAt?? this.createdAt);
+
+  }
+
+  
+
+}
